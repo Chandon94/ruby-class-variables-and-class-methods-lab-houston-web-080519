4ca2eeeca02f92d723ee4a11d6c8a51bb1378424
Chandon Porter <cneilporter94@gmail.com>
2019-08-30 15:31:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   
   @@count = 0
   @@genres = []
+  @@artists = []
   
   attr_accessor :name, :artist, :genre
   
@@ -11,6 +12,7 @@ class Song
     @genre = genre
     @@count += 1
     @@genres << genre
+    @@artists << artist
   end
   
   def self.count
@@ -20,6 +22,8 @@ class Song
   def self.genres
     @@genres.uniq
   end
+  
+  
   
       
 end
